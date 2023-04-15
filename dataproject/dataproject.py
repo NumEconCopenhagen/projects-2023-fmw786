8c4f2ae0c4a938957a72fb4017408c7203e4dea1
btn636 <124575950+btn636@users.noreply.github.com>
2023-04-15 12:08:03 +0200

    Update dataproject.py

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,114 +1,106 @@
-#Import package:
-import yfinance as yf
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
+class eeff:
 
-class eeff
+    def down():
+        #Choose 20 companies
+        tickers = ["AAPL", "MSFT", "AMZN", "TSLA", "CLX", "CMA", "CTRA", "ELV", "ESS", "EXR", "FANG", "FMC", "FTV", "GOOGL", "GS", "HAL", "IVZ", "JPM", "NWL", "NFLX", ]
 
-def down():
-    #Choose 20 companies
-    tickers = ["AAPL", "MSFT", "AMZN", "TSLA", "CLX", "CMA", "CTRA", "ELV", "ESS", "EXR", "FANG", "FMC", "FTV", "GOOGL", "GS", "HAL", "IVZ", "JPM", "NWL", "NFLX", ]
+        #Dowload data for the last 10 years
+        start_date = "2013-01-01"
+        end_date = "2023-01-01"
 
-    #Dowload data for the last 10 years
-    start_date = "2013-01-01"
-    end_date = "2023-01-01"
+        ohlc = yf.download(tickers, start=start_date, end=end_date)
+        prices = ohlc["Adj Close"].dropna(how="all")
+        prices.tail()
 
-    ohlc = yf.download(tickers, start=start_date, end=end_date)
-    prices = ohlc["Adj Close"].dropna(how="all")
-    prices.tail()
+        #Plot data
+        prices[prices.index >= "2008-01-01"].plot(figsize=(15,8));
 
-    #Plot data
-    prices[prices.index >= "2008-01-01"].plot(figsize=(15,8));
+    def cov():
+        #Import
+        import pypfopt
+        pypfopt.__version__
 
-def cov():
-    #Import
-    import pypfopt
-    pypfopt.__version__
+        #Covariance
+        from pypfopt import risk_models
+        from pypfopt import plotting
 
-    #Covariance
-    from pypfopt import risk_models
-    from pypfopt import plotting
+        sample_cov = risk_models.sample_cov(prices, frequency=252)
+        sample_cov
 
-    sample_cov = risk_models.sample_cov(prices, frequency=252)
-    sample_cov
+        #Plot covariance
+        plotting.plot_covariance(sample_cov, plot_correlation=True);
 
-    #Plot covariance
-    plotting.plot_covariance(sample_cov, plot_correlation=True);
+    def covLW():
+        #Ledoit-Wolf shrinkage (reduces extreme values in the covariance matrix)
+        S = risk_models.CovarianceShrinkage(prices).ledoit_wolf()
+        plotting.plot_covariance(S, plot_correlation=True);
 
-def covLW():
-    #Ledoit-Wolf shrinkage (reduces extreme values in the covariance matrix)
-    S = risk_models.CovarianceShrinkage(prices).ledoit_wolf()
-    plotting.plot_covariance(S, plot_correlation=True);
+    def expected_returns():
+        #Calculate expected returns
+        from pypfopt import expected_returns
 
-def expected_returns(): 
-    #Calculate expected returns
-    from pypfopt import expected_returns
+        mu = expected_returns.capm_return(prices)
+        mu
 
-    mu = expected_returns.capm_return(prices)
-    mu
+        #Plot:
+        mu.plot.barh(figsize=(10,6));
 
-    #Plot:
-    mu.plot.barh(figsize=(10,6));
+    def efficientfrontier():
+        #Import
+        from pypfopt import EfficientFrontier
 
-def efficientfrontier():
-    #Import
-    from pypfopt import EfficientFrontier
+        #Give weights
+        S = risk_models.CovarianceShrinkage(prices).ledoit_wolf()
 
-    #Give weights
-    S = risk_models.CovarianceShrinkage(prices).ledoit_wolf()
+        # You don't have to provide expected returns in this case
+        ef = EfficientFrontier(None, S, weight_bounds=(None, None))
+        ef.min_volatility()
+        weights = ef.clean_weights()
+        weights
 
-    # You don't have to provide expected returns in this case
-    ef = EfficientFrontier(None, S, weight_bounds=(None, None))
-    ef.min_volatility()
-    weights = ef.clean_weights()
-    weights
+        #Plot
+        pd.Series(weights).plot.barh();
 
-    #Plot
-    pd.Series(weights).plot.barh();
+        #Performance
+        ef.portfolio_performance(verbose=True);
 
-    #Performance
-    ef.portfolio_performance(verbose=True);
+        #Optimize
+        from pypfopt import CLA, plotting
 
-    #Optimize
-    from pypfopt import CLA, plotting
+        cla = CLA(mu, S)
+        cla.max_sharpe()
+        cla.portfolio_performance(verbose=True);
 
-    cla = CLA(mu, S)
-    cla.max_sharpe()
-    cla.portfolio_performance(verbose=True);
+        #Plot
+        ax = plotting.plot_efficient_frontier(cla, showfig=False)
 
-    #Plot
-    ax = plotting.plot_efficient_frontier(cla, showfig=False)
+    def random_portefolios():
+        #Create 1.000.000 random portefolios
+        n_samples = 1000000
+        w = np.random.dirichlet(np.ones(len(mu)), n_samples)
+        rets = w.dot(mu)
+        stds = np.sqrt((w.T * (S @ w.T)).sum(axis=0))
+        sharpes = rets / stds
 
-def random_portefolios():
-    #Create 1.000.000 random portefolios
-    n_samples = 1000000
-    w = np.random.dirichlet(np.ones(len(mu)), n_samples)
-    rets = w.dot(mu)
-    stds = np.sqrt((w.T * (S @ w.T)).sum(axis=0))
-    sharpes = rets / stds
+        print("Sample portfolio returns:", rets)
+        print("Sample portfolio volatilities:", stds)
 
-    print("Sample portfolio returns:", rets)
-    print("Sample portfolio volatilities:", stds)
+        #Plot the efficient frontier against the 1.000.000 random
+        ef = EfficientFrontier(mu, S)
 
-    #Plot the efficient frontier against the 1.000.000 random
-    ef = EfficientFrontier(mu, S)
+        fig, ax = plt.subplots()
+        plotting.plot_efficient_frontier(ef, ax=ax, show_assets=False)
 
-    fig, ax = plt.subplots()
-    plotting.plot_efficient_frontier(ef, ax=ax, show_assets=False)
+        # Find and plot the tangency portfolio
+        ef2 = EfficientFrontier(mu, S)
+        ef2.max_sharpe()
+        ret_tangent, std_tangent, _ = ef2.portfolio_performance()
 
-    # Find and plot the tangency portfolio
-    ef2 = EfficientFrontier(mu, S)
-    ef2.max_sharpe()
-    ret_tangent, std_tangent, _ = ef2.portfolio_performance()
+        # Plot random portefolios
+        ax.scatter(stds, rets, marker=".", c=sharpes, cmap="viridis_r")
 
-    # Plot random portefolios
-    ax.scatter(stds, rets, marker=".", c=sharpes, cmap="viridis_r")
-
-    # Format
-    ax.set_title("Efficient Frontier and Random Portfolios")
-    ax.legend()
-    plt.tight_layout()
-    plt.show()
-
-pass
+        # Format
+        ax.set_title("Efficient Frontier and Random Portfolios")
+        ax.legend()
+        plt.tight_layout()
+        plt.show()
