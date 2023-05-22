f73846f93de537b44fe7c63b5002b2cc6ebe7da6
fmw786 <fmw786@econ.ku.dk>
2023-05-22 17:08:38 +0200

    Hej

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -30,13 +30,13 @@ class HumanCapitalSolowModelClass:
         par.phi = 1/3
         par.s_K = 0.2
         par.s_H = 0.15
-        par.n = 0.0
+        par.n = 0.00
         par.g = 0.015
         par.delta = 0.06
 
         # c. initial
-        par.K_lag_ini = 1.0
-        par.H_lag_ini = 1.0
+        par.K_ini = 10
+        par.H_ini = 10
 
         # d. misc
         par.solver = 'broyden' # solver for the equation system, 'broyden'.
@@ -52,20 +52,21 @@ class HumanCapitalSolowModelClass:
         for varname in allvarnames:
             path.__dict__[varname] =  np.nan*np.ones(par.Tpath)
 
-    def trans_eqs(self):
-        """Input is the values of the parameters as described in the analytical section. 
-        Output is the two transition equations for k and h respectivly """
+    def trans_eqs(self, K, H):
+        """Input is the values of K and H, and the values of the parameters as described in the analytical section. 
+        Output is the two transition equations for K and H, respectively."""
 
         par = self.par
         path = self.path
 
-        # Transition equation for k:
-        trans_eq_k = (1 / ((1 + par.n) * (1 + par.g))) * (par.s_K * par.k**par.alpha * par.h**par.phi + (1 - par.delta) * par.k)-par.k
+        # Transition equation for K:
+        trans_eq_K = (1 / ((1 + par.n) * (1 + par.g))) * (par.s_K * K**par.alpha * H**par.phi + (1 - par.delta) * K) - K
 
-        # Transition equation for h:
-        trans_eq_h = (1 / ((1 + par.n) * (1 + par.g))) * (par.s_H * par.k**par.alpha * par.h**par.phi + (1 - par.delta) * par.h)-par.h
-    
-        return trans_eq_k, trans_eq_h
+        # Transition equation for H:
+        trans_eq_H = (1 / ((1 + par.n) * (1 + par.g))) * (par.s_H * K**par.alpha * H**par.phi + (1 - par.delta) * H) - H
+
+        return trans_eq_K, trans_eq_H
+
 
     def find_steady_state(self, do_print=True):
         """Find steady state"""
@@ -74,14 +75,80 @@ class HumanCapitalSolowModelClass:
         path = self.path
 
         # Solving the model using optimize.root:
-        objective = lambda x: [HumanCapitalSolowModelClass.trans_eqs(x[0],x[1], self)]
-        solution = optimize.root(objective,[1,1], method='broyden1')
+        objective = lambda x: self.trans_eqs(x[0], x[1])
+        initial_guess = [par.K_ini, par.H_ini]  # Use initial values from the setup method
+        solution = optimize.root(objective, initial_guess, method='broyden1')
         num_solution = solution.x
+
+        # Extracting the values of trans_eq_K and trans_eq_H
+        trans_eq_K, trans_eq_H = self.trans_eqs(num_solution[0], num_solution[1])
 
         # Calculating steady state value of y
         ss_y = num_solution[0]**par.alpha * num_solution[1]**par.phi
 
         if do_print:
-            print(f"The steady state value of k is: {num_solution[0]}")
-            print(f"The steady state value of h is: {num_solution[1]}")
-            print(f"The steady state value of y is: {ss_y}")
+            print(f"The steady state value of \u0303k* is: {num_solution[0]:.5f}")
+            print(f"The steady state value of \u0303h* is: {num_solution[1]:.5f}")
+            print(f"The steady state value of \u0303y* is: {ss_y:.5f}")
+
+        return num_solution[0], num_solution[1], ss_y
+    
+    def simulate_transition_path(self):
+        """Simulate transition path for k, h and y"""
+
+        par = self.par
+        path = self.path
+
+        # Time
+        T = np.arange(0, par.Tpath)
+
+        # Variables for transition path
+        Kt = np.zeros(par.Tpath)
+        Ht = np.zeros(par.Tpath)
+        Yt = np.zeros(par.Tpath)
+        Lt = np.zeros(par.Tpath)
+        At = np.zeros(par.Tpath)
+
+        # Production function
+        def production(K, H, L, A):
+            return K**par.alpha * H**par.phi * (A*L)**(1 - par.alpha - par.phi)
+
+        # Initial values
+        Kt[0] = 1
+        Ht[0] = 1
+        Lt[0] = 1
+        At[0] = 1
+        Yt[0] = production(Kt[0], Ht[0], Lt[0], At[0])
+
+        # Model simulation
+        for t in range(1, par.Tpath):
+            Kt[t] = par.s_K * Yt[t-1] + (1 - par.delta) * Kt[t-1]
+            Ht[t] = par.s_H * Yt[t-1] + (1 - par.delta) * Ht[t-1]
+            Lt[t] = Lt[t-1] * (1 + par.n)
+            At[t] = At[t-1] * (1 + par.g)
+            Yt[t] = production(Kt[t], Ht[t], Lt[t], At[t])
+
+        # Define tilde variables:
+        kt_tilde = Kt / (At * Lt)
+        ht_tilde = Ht / (At * Lt)
+        yt_tilde = Yt / (At * Lt)
+
+        return kt_tilde, ht_tilde, yt_tilde
+    
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+        
