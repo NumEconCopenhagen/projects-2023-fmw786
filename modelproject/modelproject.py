fe36dc5ad079e18b68652bed2de5b3f6df3c6a28
fmw786 <fmw786@econ.ku.dk>
2023-05-23 16:27:37 +0200

    jep

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -39,7 +39,6 @@ class HumanCapitalSolowModelClass:
         par.H_ini = 10
 
         # d. misc
-        par.solver = 'broyden' # solver for the equation system, 'broyden'.
         par.Tpath = 500 # length of transition path.
 
     def allocate(self):
@@ -58,12 +57,12 @@ class HumanCapitalSolowModelClass:
 
         par = self.par
         path = self.path
-
+        
         # Transition equation for K:
         trans_eq_K = (1 / ((1 + par.n) * (1 + par.g))) * (par.s_K * K**par.alpha * H**par.phi + (1 - par.delta) * K) - K
 
         # Transition equation for H:
-        trans_eq_H = (1 / ((1 + par.n) * (1 + par.g))) * (par.s_H * K**par.alpha * H**par.phi + (1 - par.delta) * H) - H
+        trans_eq_H = (1 / ((1 + par.n) * (1 + par.g))) * (par.s_H * K**par.alpha * H**par.phi + (1 - par.delta) * H) - H 
 
         return trans_eq_K, trans_eq_H
 
@@ -74,6 +73,7 @@ class HumanCapitalSolowModelClass:
         par = self.par
         path = self.path
 
+        
         # Solving the model using optimize.root:
         objective = lambda x: self.trans_eqs(x[0], x[1])
         initial_guess = [par.K_ini, par.H_ini]  # Use initial values from the setup method
@@ -135,20 +135,76 @@ class HumanCapitalSolowModelClass:
 
         return kt_tilde, ht_tilde, yt_tilde
     
+    def simulate_steady_state_shock(self):
+        """ Simulate the effect of a permanent increase in the savings rate for human capital """
 
+        par = self.par
+        path = self.path
 
+        # Time
+        par.newTpath = 1000  # length new of transition path.
+        T = np.arange(0, par.newTpath)
 
+        # New savings rate
+        par.new_s_H = 0.2
 
+        # Variables for transition path
+        Kn = np.zeros(par.newTpath)
+        Hn = np.zeros(par.newTpath)
+        Yn = np.zeros(par.newTpath)
+        Ln = np.zeros(par.newTpath)
+        An = np.zeros(par.newTpath)
 
+        # Production function
+        def production(K, H, L, A):
+            return K ** par.alpha * H ** par.phi * (A * L) ** (1 - par.alpha - par.phi)
 
+        # Initial values
+        Kn[0] = 1
+        Hn[0] = 1
+        Ln[0] = 1
+        An[0] = 1
+        Yn[0] = production(Kn[0], Hn[0], Ln[0], An[0])
+
+        # Model simulation
+        for t in range(1, par.newTpath):
+            if t >= 750:
+                s_H = par.new_s_H  # Use the new_s_H parameter at period 750
+            else:
+                s_H = par.s_H  # Use the original s_H parameter for other periods
+            Kn[t] = par.s_K * Yn[t - 1] + (1 - par.delta) * Kn[t - 1]
+            Hn[t] = s_H * Yn[t - 1] + (1 - par.delta) * Hn[t - 1]
+            Ln[t] = Ln[t - 1] * (1 + par.n)
+            An[t] = An[t - 1] * (1 + par.g)
+            Yn[t] = production(Kn[t], Hn[t], Ln[t], An[t])
+
+        # Define tilde variables:
+        kn_tilde = Kn / (An * Ln)
+        hn_tilde = Hn / (An * Ln)
+        yn_tilde = Yn / (An * Ln)
+
+        return kn_tilde, hn_tilde, yn_tilde
     
 
+    def steady_state_consumption(self):
+        """Find steady state consumption"""
 
+        par = self.par
+        path = self.path
 
+        # Solving the model using optimize.root:
+        objective = lambda x: self.trans_eqs(x[0], x[1])
+        initial_guess = [par.K_ini, par.H_ini]  # Use initial values from the setup method
+        solution = optimize.root(objective, initial_guess, method='broyden1')
+        num_solution = solution.x
 
+        # Extracting the values of trans_eq_K and trans_eq_H
+        trans_eq_K, trans_eq_H = self.trans_eqs(num_solution[0], num_solution[1])
 
+        # Calculating steady state value of y
+        ss_y = num_solution[0]**par.alpha * num_solution[1]**par.phi
 
+        # Calculating steady state value of c
+        ss_c = (1- par.s_K - par.s_H) * ss_y
 
-
-
-        
+        return num_solution[0], num_solution[1], ss_y, ss_c
